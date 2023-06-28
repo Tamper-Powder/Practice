0d13d1528e1f3b2772b82af342f0fceac28f2721
Tamper-Powder <tamper.powder@gmail.com>
2023-06-28 12:39:23 +0300

    String

---
+++ project2.js
@@ -0,0 +1,11 @@
+const phoneBook ={
+    list:{
+        "John": 123456789,
+        "Jane Doe": 987654321,
+        "Jim Smith": 111111111
+    },
+   };
+      for (const name in phoneBook.list){
+        console.log(name,phoneBook.list[name]); 
+        // console.log(phoneBook.list[name]);
+    }
